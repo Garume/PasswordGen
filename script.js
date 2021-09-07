115b99db8dd9937be904e30b53e2c9116be77740
desuboruto <desuboruto@gmail.com>
2021-09-07 16:52:21 +0900

    除外を追加

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const passwordListElement = document.getElementById('passwordList')
+
+const baseExcludeTexts = ['o','O','0','I','l','1']
+document.getElementById('excludeText').value = baseExcludeTexts.join(',')
+
 const passwordTexts = [],
     passwordLength = 6,
     generateNumberOfTimes = 3;
-
 for (let i = 0; i < 26; i++) {
     passwordTexts.push(String.fromCharCode('a'.charCodeAt(0) + i));
     passwordTexts.push(String.fromCharCode('A'.charCodeAt(0) + i));
@@ -16,10 +19,14 @@ document.getElementById('generateButton').addEventListener('click', e => {
     e.preventDefault();
 
     passwordListElement.innerHTML = '';
+
+    const excludeTexts = document.forms.inputs.excludeText.value.split(','),
+        excludePasswordTexts = passwordTexts.filter(text => !(excludeTexts.includes(String(text))));
+
     for (let index = 0; index < generateNumberOfTimes; index++) {
         let passwords = '';
         for (let index = 0; index < passwordLength; index++) {
-            passwords += passwordTexts[Math.floor(Math.random() * passwordTexts.length)];
+            passwords += excludePasswordTexts[Math.floor(Math.random() * excludePasswordTexts.length)];
         }
         const li = document.createElement('li'),
             input = document.createElement('input');
