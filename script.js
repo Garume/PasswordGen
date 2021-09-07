ffc71c43d549216355484d503abcfa6b14629322
desuboruto <desuboruto@gmail.com>
2021-09-07 19:00:35 +0900

    構成する文字を選択可能、モーダルウィンドウの追加

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
-const passwordListElement = document.getElementById('passwordList')
+MicroModal.init({
+    openClass: 'is-open'
+});
 
-const baseExcludeTexts = ['o','O','0','I','l','1']
+const passwordListElement = document.getElementById('passwordList')
+const baseExcludeTexts = ['o', 'O', '0', 'I', 'l', '1']
 document.getElementById('excludeText').value = baseExcludeTexts.join(',')
 
-const passwordTexts = [],
-    passwordLength = 6,
+const passwordLength = 6,
     generateNumberOfTimes = 3;
-for (let i = 0; i < 26; i++) {
-    passwordTexts.push(String.fromCharCode('a'.charCodeAt(0) + i));
-    passwordTexts.push(String.fromCharCode('A'.charCodeAt(0) + i));
-}
-for (let index = 0; index < 10; index++) {
-    passwordTexts.push(index);
-}
-console.log(passwordTexts);
 
 document.getElementById('generateButton').addEventListener('click', e => {
     e.preventDefault();
 
-    passwordListElement.innerHTML = '';
+    const isIncludeNum = document.forms.inputs.includeNumber.checked,
+        isIncludeEngLower = document.forms.inputs.includeEnglishLower.checked,
+        isIncludeEngUpper = document.forms.inputs.includeEnglishUpper.checked;
 
-    const excludeTexts = document.forms.inputs.excludeText.value.split(','),
-        excludePasswordTexts = passwordTexts.filter(text => !(excludeTexts.includes(String(text))));
+    if (isIncludeNum || isIncludeEngLower || isIncludeEngUpper) {
+        passwordListElement.innerHTML = '';
 
-    for (let index = 0; index < generateNumberOfTimes; index++) {
-        let passwords = '';
-        for (let index = 0; index < passwordLength; index++) {
-            passwords += excludePasswordTexts[Math.floor(Math.random() * excludePasswordTexts.length)];
+        const passwordTexts = [];
+        if (isIncludeEngLower) {
+            for (let i = 0; i < 26; i++) {
+                passwordTexts.push(String.fromCharCode('a'.charCodeAt(0) + i));
+            }
         }
-        const li = document.createElement('li'),
-            input = document.createElement('input');
-        input.value = passwords;
-        li.appendChild(input);
-        passwordListElement.appendChild(li);
+        if (isIncludeEngUpper) {
+            for (let i = 0; i < 26; i++) {
+                passwordTexts.push(String.fromCharCode('A'.charCodeAt(0) + i));
+            }
+        }
+        if (isIncludeNum) {
+            for (let index = 0; index < 10; index++) {
+                passwordTexts.push(index);
+            }
+        }
+
+        const excludeTexts = document.forms.inputs.excludeText.value.split(','),
+            excludePasswordTexts = passwordTexts.filter(text => !(excludeTexts.includes(String(text))));
+
+        for (let index = 0; index < generateNumberOfTimes; index++) {
+            let passwords = '';
+            for (let index = 0; index < passwordLength; index++) {
+                passwords += excludePasswordTexts[Math.floor(Math.random() * excludePasswordTexts.length)];
+            }
+            const li = document.createElement('li'),
+                input = document.createElement('input');
+            input.value = passwords;
+            li.appendChild(input);
+            passwordListElement.appendChild(li);
+        }
+    } else {
+        MicroModal.show('modal-1');
     }
 })
